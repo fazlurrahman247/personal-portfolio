f426e1b91324b9798331d14e7e9bf2ce20f8f62b
Fazlur Rahman <fazlurrr247@gmail.com>
2026-07-16 09:55:27 +0700

    push

---
+++ portfolioDesain.js
@@ -0,0 +1,56 @@
+// portfolioDesain.js
+// Menyimpan data gambar portofolio dalam bentuk array per kategori rasio.
+// Saat kamu menambah desain baru, tinggal tambah entry di bawah.
+
+window.portfolioData = {
+  "16:9": [
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-31c32073-ea08-4140-87c6-654df3a08c6b.jpg",
+      alt: "16:9 - Web Design",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-a84b35ca-2a0e-40aa-a0c1-1a93e2325220.jpg",
+      alt: "16:9 - Packaging",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-3ec27ceb-b2e7-4274-8347-014dc227c6df.jpg",
+      alt: "16:9 - Identity",
+    },
+  ],
+  "4:5": [
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-0ced460d-e69d-4a9c-83a2-f7d5adf8505c.png",
+      alt: "4:5 - Threads Poster",
+    },
+  ],
+  "9:16": [
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-3ee44e5a-eae5-4f5b-8bb4-0ac652f7a8ed.jpg",
+      alt: "BMW",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-8ee68729-e0a6-4958-a3c0-759536e835b7.jpg",
+      alt: "9:16 - Mobile UI",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-0c3af717-a9f4-40a0-a757-9fc939b80467.jpg",
+      alt: "apa itu ramadan",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-67d1c1f6-1a68-4481-8c58-bf5f784ddc5f.jpg",
+      alt: "9:16 - Poster Takoyaki",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-96255548-0d43-45e6-b923-278d80d52e8a.jpg",
+      alt: "9:16 - kalender",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-5e769719-8822-420e-ad87-2efbd91b4f14.jpg",
+      alt: "9:16 - Poster Menu makanan 25% off",
+    },
+    {
+      src: "https://cdn.phototourl.com/free/2026-07-16-0bed3047-9aff-4b58-abe3-7da06ca0cb8f.jpg",
+      alt: "9:16 - Quote ayat",
+    },
+  ],
+};
